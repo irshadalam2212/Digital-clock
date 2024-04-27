44ceb9b53ff01e6627c709beffe78829638c9557
Md Irshad Alam <87612758+irshadalam2212@users.noreply.github.com>
2024-04-27 13:02:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// let clock = document.querySelector('.clock')
+
+
+
+// setInterval(function(){
+//     let date = new Date();
+//     // console.log(date.toLocaleTimeString());
+//     clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
+
+
+const clock = document.querySelector('.clock')
+
+setInterval(function(){
+    let d   ate = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000)
